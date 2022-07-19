ad70b757f33e8740186f5ae60438599a4c6394dd
Kiransai321 <94594083+Kiransai321@users.noreply.github.com>
2022-07-19 10:06:46 +0530

    Update s3_create.py

--- s3_create.py
+++ s3_create.py
@@ -4,7 +4,7 @@ client = boto3.client('s3')
 
 response = client.create_bucket(
    # ACL='private'|'public-read'|'public-read-write'|'authenticated-read',
-    Bucket='kiransai3214567788',
+    #Bucket='kiransai3214567788',
     CreateBucketConfiguration={
         'LocationConstraint': 'us-east-2'
     },
